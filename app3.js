94bf5fa833f09a59d01f796f1d8db8f19928553a
itsmedion <dionosalim@gmail.com>
2024-08-06 19:59:32 +0700

    percobaan file html terpisah

--- app3.js
+++ app3.js
@@ -1,4 +1,5 @@
 const http= require('http')
+const  fs = require('fs')
 
 let server = http.createServer( function(request, respon) {
    if(request.url == '/') {
@@ -6,15 +7,25 @@ let server = http.createServer( function(request, respon) {
     respon.write('<h1>Halaman Beranda</h1>')
         respon.end()
    } else if (request.url == '/profil') {
-    respon.writeHead(200, {'Content-type': 'text/html'})
-    respon.write('<h1>Ini adalah profil saya</h1>')
-        respon.end()
-   } else {
+    fs.createReadStream(',/view/profil.html').pipe(respon)
+    } else if (request.url == '/berita') {
+        respon.writeHead(200, {'Content-type': 'text/html'})
+        respon.write(
+        `<h1>Berita Terupdate dari kami:</h1><hr>
+            Judul Berita 1 : Lorem Ipsum bla bla bla <br>
+            Judul Berita 2 : Lorem Ipsum bla bla bla <br>
+            Judul Berita 3 : Lorem Ipsum bla bla bla <br>
+            Judul Berita 4 : Lorem Ipsum bla bla bla <br>`
+        )
+            respon.end()
+    
+   }
+    else {
     respon.writeHead(200, {'Content-type': 'text/html'})
     respon.write('<h1>404 Not Found</h1>')
         respon.end()
-   }
-})
+   }}
+)
 
 server.listen(3000, function() {
     console.log('Server sudah siap, buka http://localhost:3000');
